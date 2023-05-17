45a99befb4ca0fc59790a226164746e0288c0af3
dailyrang <69h9@naver.com>
2023-05-17 17:08:26 +0900

    댓글 입력 및 댓글 리스트 출력 기능 추가

---
+++ src/main/java/com/rubato/home/RubatoHomeProject230517Application.java
@@ -0,0 +1,13 @@
+package com.rubato.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubatoHomeProject230517Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubatoHomeProject230517Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/rubato/home/controller/HomeController.java
@@ -0,0 +1,111 @@
+package com.rubato.home.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.rubato.home.dao.IDao;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@RequestMapping(value = "/index")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping(value = "/board_list")
+	public String board_list(Model model) {
+		
+		IDao dao = sqlSession.getMapper(IDao.class);
+		model.addAttribute("list", dao.boardListDao());
+		model.addAttribute("totalCount", dao.boardTotalCountDao());		
+		
+		return "board_list";
+	}
+	
+	@RequestMapping(value = "/board_write")
+	public String board_write() {
+		return "board_write";
+	}
+	
+	@RequestMapping(value = "/board_view")
+	public String board_view(HttpServletRequest request, Model model) {
+		
+		IDao dao = sqlSession.getMapper(IDao.class);
+		
+		dao.boardHitDao(request.getParameter("bnum"));//조회수 증가
+		
+		model.addAttribute("boardDto", dao.boardContentViewDao(request.getParameter("bnum")));
+		
+		model.addAttribute("replyList", dao.replyListDao(request.getParameter("bnum")));
+		
+		return "board_view";
+	}
+	
+	@RequestMapping(value = "/board_writeOk")
+	public String board_writeOk(HttpServletRequest request) {
+		
+		String bname = request.getParameter("bname");
+		String btitle = request.getParameter("btitle");
+		String bcontent = request.getParameter("bcontent");
+		
+		IDao dao = sqlSession.getMapper(IDao.class);
+		
+		dao.boardWriteDao(bname, btitle, bcontent, "정회원", 0);
+		
+		return "redirect:board_list";
+	}
+	
+	@RequestMapping(value = "/search_list")
+	public String search_list(HttpServletRequest request, Model model) {
+		
+		String searchOption = request.getParameter("searchOption");
+		String keyword = request.getParameter("keyword");
+		
+		IDao dao = sqlSession.getMapper(IDao.class);
+		
+		if(searchOption.equals("title" )) {
+			model.addAttribute("list", dao.boardSearchTitleDao(keyword));
+			model.addAttribute("totalCount", dao.boardSearchTitleDao(keyword).size());
+		} else if(searchOption.equals("content")) {
+			model.addAttribute("list", dao.boardSearchContentDao(keyword));
+			model.addAttribute("totalCount", dao.boardSearchContentDao(keyword).size());
+		} else {
+			model.addAttribute("list", dao.boardSearchWriterDao(keyword));
+			model.addAttribute("totalCount", dao.boardSearchWriterDao(keyword).size());
+		}
+		
+		return "board_list";
+	}
+	
+	@RequestMapping(value = "/reply_write")
+	public String reply_write(HttpServletRequest request, Model model) {
+		
+		
+		IDao dao = sqlSession.getMapper(IDao.class);
+		
+		dao.replyWriteDao(request.getParameter("rcontent"), request.getParameter("rorinum"));		
+		
+		dao.replyCountDao(request.getParameter("rorinum"));//원글의 댓글 수를 1증가		
+		
+		model.addAttribute("boardDto", dao.boardContentViewDao(request.getParameter("rorinum")));
+		
+		model.addAttribute("replyList", dao.replyListDao(request.getParameter("rorinum")));
+				
+		return "board_view";
+	}
+	
+	
+	
+	
+	
+
+}
